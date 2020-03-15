801398c1c0e68cc833f389413f786a6dbdee4978
Boyuan Zhang <54850925+zhangby222@users.noreply.github.com>
2020-03-15 18:30:20 -0400

    Update RF_FeatureImportance.py

--- RF_FeatureImportance.py
+++ RF_FeatureImportance.py
@@ -1,2 +1,5 @@
 # 1. The 'FeatureImportance' algorithm designed by JH
-# 2.
+# RF has almost no assumptions
+# 2. Industry methods of doing feature importance: PCA distance of clusters. : Classical statistical techniques.
+# Totally the wrong way to do things?
+
